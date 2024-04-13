96db08c08604d192bf60b3f8b0cf03b8e3411fda
atharva <atharvak.8023@gmail.com>
2024-04-13 13:05:09 +0530

    login component

--- my-project/src/pages/auth.jsx
+++ my-project/src/pages/auth.jsx
@@ -3,9 +3,9 @@ import React from 'react';
 const Auth = () => { // Correct the component name to Auth
   return (
     <div>
-        auth
+      <h1>login and register components</h1>
     </div>
-  )
+  );
 }
 
 export default Auth;
