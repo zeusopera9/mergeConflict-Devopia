db701c3d651260b962589af15344314d495343b9
Yakshit Poojary <yakshitpoojary2004@gmail.com>
2024-04-13 16:05:53 +0530

    minor

--- my-project/src/components/Auth/Register.jsx
+++ my-project/src/components/Auth/Register.jsx
@@ -11,7 +11,7 @@ const Register = () => {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Register
             </h1>
             <form className="space-y-4 md:space-y-6" action="#">
               <div className="grid grid-cols-2 gap-6">
@@ -86,7 +86,13 @@ const Register = () => {
                 Sign Up
               </button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
-                Or{" "}
+                <a
+                  href="/auth"
+                  className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+                >
+                  Login 
+                </a>
+                {" "}Or{" "}
                 <a
                   href="/teacher"
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
