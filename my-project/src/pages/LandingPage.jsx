48d86c7caccfddd67cffd14426ed94457eb14b00
zeusopera9 <zaidalimerchant@yahoo.com>
2024-04-13 14:12:00 +0530

    Fix

--- my-project/src/pages/LandingPage.jsx
+++ my-project/src/pages/LandingPage.jsx
@@ -4,7 +4,6 @@ import Welcome from "../components/Landing/Welcome"
 const LandingPage = () => {
   return (
     <>
-      {/* <Header /> */}
       <div>
         This is the Landing Page
         <Welcome />
