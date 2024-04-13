7b9d7433eb21d8ef16542ec1f1da56878c182d98
zeusopera9 <zaidalimerchant@yahoo.com>
2024-04-13 12:01:24 +0530

    FS

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Welcome from './components/Welcome'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -11,6 +12,7 @@ function App() {
       <p>
         <h1 className="text-3xl font-bold underline">
           Hello world!
+          <Welcome />
         </h1>
       </p>
     </>
