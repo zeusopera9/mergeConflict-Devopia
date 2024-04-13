2cec387daeeeb641c47eb4c26a66c77e112dfa63
atharva <atharvak.8023@gmail.com>
2024-04-13 15:56:09 +0530

    backend

---
+++ backend/main.py
@@ -0,0 +1,20 @@
+from flask import Flask
+from flask_pymongo import PyMongo
+from pymongo.errors import ConnectionFailure
+
+mongoConnectionString = "mongodb+srv://atharvak8023:8ODzB3dtMAlWhUe4@mergeconflict.thy4pad.mongodb.net/mergeConflict"
+
+app = Flask(__name__)
+app.config["MONGO_URI"] = mongoConnectionString
+mongo = PyMongo(app)
+
+@app.route('/')
+def hello():
+    try:
+        mongo.db.students.insert_one({'name': 'John Doe'})
+        return 'Hello, World!'
+    except ConnectionFailure as e:
+        return f"Failed to connect to MongoDB: {e}"
+
+if __name__ == '__main__':
+    app.run(debug=True)
