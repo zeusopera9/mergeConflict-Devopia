b75f9cfd6ee4c8da3039d8f83543e26cef4e0d05
atharva <atharvak.8023@gmail.com>
2024-04-13 12:15:07 +0530

    route of landing page

--- my-project/src/App.jsx
+++ my-project/src/App.jsx
@@ -3,19 +3,19 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import Welcome from './components/Welcome'
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import LandingPage from './pages/LandingPage';
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
-      <p>
-        <h1 className="text-3xl font-bold underline">
-          Hello world!
-          <Welcome />
-        </h1>
-      </p>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<LandingPage />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
