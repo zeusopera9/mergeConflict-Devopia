bfc8ae51b8aa5c7b749dc22286893f66a34a9fa0
zeusopera9 <zaidalimerchant@yahoo.com>
2024-04-13 15:21:18 +0530

    Scroll Fix

--- my-project/src/components/Auth/Login.jsx
+++ my-project/src/components/Auth/Login.jsx
@@ -5,7 +5,7 @@ const Login = () => {
         <section className="min-h-screen flex justify-center items-center">
           <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
             <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
-              <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+              <div className="p-6 space-y-4 md:space-y-6 sm:p-8" style={{ maxHeight: '100vh', overflowY: 'auto' }}>
                 <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
                   Sign in to your account
                 </h1>
